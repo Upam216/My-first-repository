3949515819af46d0868fabcb1776fe7ae9735c2d
Upam216 <110330386+Upam216@users.noreply.github.com>
2022-07-31 19:52:05 +0530

    Add files via upload
    My first programs

---
+++ 2 nd programming import excel code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas
+
+
+# In[3]:
+
+
+pandas.read_excel(r"C:\Users\Upam kumar\Downloads\Book.xlsx")
+
+
+# In[ ]:
+
+
+
+
---
+++ First programming.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+x=6
+y=7
+Z=x+7
+
+print(Z)
+
+
+# In[ ]:
+
+
+
+
