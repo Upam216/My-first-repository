a879e0bb4966f34d0e20c9d993c675430f452fbf
Upam216 <110330386+Upam216@users.noreply.github.com>
2022-07-31 20:10:54 +0530

    Add files via upload

---
+++ 4 program import tsv into jupiter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+pd.read_csv('students.tsv',sep="\t")
+
---
+++ import data from csv file to jupiter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+pd.read_csv(r"C:\Users\Upam kumar\Documents\My data sheet - Sheet3.csv")
+
+
+# In[ ]:
+
+
+
+
