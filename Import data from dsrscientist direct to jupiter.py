acb8f17cfdd189020e2ad9b933012354c360a77d
Upam216 <110330386+Upam216@users.noreply.github.com>
2022-07-31 20:24:38 +0530

    Add files via upload

---
+++ Import data from dsrscientist direct to jupiter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/DSData/master/Advertising.csv")
+
+
+# In[ ]:
+
+
+
+
